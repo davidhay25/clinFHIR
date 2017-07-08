adae081e92997a02327973d71fe233c862531335
david hay <david.hay25@gmail.com>
2017-07-08 10:47:28 -0700

    still working on recorder

--- js/sbHistoryCtl.js
+++ js/sbHistoryCtl.js
@@ -5,5 +5,7 @@ angular.module("sampleApp")
 
             $scope.selectItem = function(item) {
                 $scope.selectedItem = item
+                console.log(item.details.value)
+                $scope.selectedItemValue = item.details.value;
             }
     })
