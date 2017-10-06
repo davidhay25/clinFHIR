b1eb86476f783e67c0707d95cda7d5329252b618
David Hay <david.hay25@gmail.com>
2017-10-06 15:57:14 +1300

    insert node working

--- js/newBuilderCtl.js
+++ js/newBuilderCtl.js
@@ -17,41 +17,96 @@ angular.module("sampleApp")
                 var SD = data.data;
                 newBuilderSvc.makeTree(SD).then(
                     function(vo) {
-
-
-                        $('#SDtreeView').jstree('destroy');
-                        $('#SDtreeView').jstree(
-                            {'core': {'multiple': false, 'data': vo.treeData, 'themes': {name: 'proton', responsive: true}}}
-                        ).on('select_node.jstree', function (e, data) {
-
-                            //clear specific properties
-                            delete $scope.selectedNode;
-                            delete $scope.currentDT;
-                            $scope.$broadcast('setDT',null);      //sets an event to reset the data-entry form
-
-                            if (data.node && data.node.data && data.node.data) {
-                                $scope.selectedNode = data.node;
-                                processNode($scope.selectedNode);
-                            }
-                            $scope.$digest();
-                        })
+                        $scope.treeData = vo.treeData;
+                        drawTree(vo.treeData)
 
                     }
                 )
             }
         );
 
+        function drawTree(treeData) {
+            $('#SDtreeView').jstree('destroy');
+            $('#SDtreeView').jstree(
+                {'core': {'multiple': false, 'data': treeData, 'themes': {name: 'proton', responsive: true}}}
+            ).on('select_node.jstree', function (e, data) {
+
+                //clear specific properties
+                delete $scope.selectedNode;
+                delete $scope.currentDT;
+                $scope.$broadcast('setDT',null);      //sets an event to reset the data-entry form
+
+                if (data.node && data.node.data && data.node.data) {
+                    $scope.selectedNode = data.node;
+                    processNode($scope.selectedNode);
+                }
+                $scope.$digest();
+            })
+        }
+
+        //called on a BBE that is repeatable. we want to make a copy of that node and all childnodes...
+            //todo note that this only works for direct children right now
+        $scope.addNewNode = function() {
+            var nodeId = $scope.selectedNode.id;
+            //find the children that we'll need to duplicate. Right now, it's not repeatible - so fail for carePlan for example..
+            var arNewNodes = [];
+            var newParent = {data:{}}; //angular.copy($scope.selectedNode)
+            newParent.text = $scope.selectedNode.text;
+            newParent.data.meta = angular.copy($scope.selectedNode.data.meta)
+            //delete newParent.parents;
+            newParent.parent = $scope.selectedNode.parent;
+            var inx = $scope.treeData.length
+            newParent.id = 'id'+inx ;
+            newParent.data.meta.index = 'inx'+inx     //this is used to distinguish this 'branch' from the others
+            //newParent.text += 'x'
+            inx++;
+            arNewNodes.push(newParent);
+            var insertPoint = - 1;
+            $scope.treeData.forEach(function (node,pos) {
+                if (node.parent == nodeId) {
+                    insertPoint = pos;     //finds the last element with this one as the parent
+                }
+                if (node.parent == nodeId) {
+                    var newChild = {data:{}}; //angular.copy(node);
+                    newChild.text = node.text;
+                    newChild.icon = node.icon;
+                    newChild.data.meta = angular.copy(node.data.meta)
+                    newChild.data.meta.index = newParent.data.meta.index;
+                    newChild.id = 'id'+inx ;
+                    inx++;
+                    newChild.parent = newParent.id;
+                    newChild.text += 'x'
+
+                    arNewNodes.push(newChild);
+                }
+
+            });
+
+            var ar = $scope.treeData;
+
+            for (var j=arNewNodes.length-1; j > -1; j--) {
+                var nodeToInsert= arNewNodes[j];
+             //   $scope.treeData.splice(insertPoint-1,0,nodeToInsert)
+            }
+          //  $scope.treeData =  $scope.treeData.concat(arNewNodes);
+
+            $scope.treeData = ar.slice(0,insertPoint+1).concat(arNewNodes).concat(ar.slice(insertPoint+1));
+
+          //  $scope.treeData =  $scope.treeData.slice(concat(arNewNodes);
+            drawTree($scope.treeData)
+
+        };
 
         //once a node has been selected in the tree
         function processNode(node){
-            $scope.selectedElement = {};     //what is displayed...
-            $scope.selectedElement.meta = node.data.meta;
+         //   $scope.selectedElement = {};     //what is displayed...
+          //  $scope.selectedElement.meta = node.data.meta;
         }
 
         //to show the data entry form...
         $scope.showDEForm = function(dt){
             $scope.currentDT = dt;
-            var meta = $scope.selectedElement.meta;     //the specific meta node
+            var meta = $scope.selectedNode.data.meta;     //the specific meta node
 
             if (meta.vs) {
                 //this element has a valueSet binding...
@@ -72,7 +127,7 @@ angular.module("sampleApp")
                 if (dt.substr(0,1) == dt.substr(0,1).toUpperCase()) {
                     value = $scope.complexData;
                 }
-                addData(dt,value);
+               // addData(dt,value);
             }
 
             $scope.$broadcast('setDT',dt);      //sets an event to display the data-entry form
@@ -99,7 +154,7 @@ angular.module("sampleApp")
         };
 
         var addData = function(dt,value){
-            var meta = $scope.selectedElement.meta;     //the specific meta node
+            var meta = $scope.selectedNode.data.meta;     //the specific meta node
             //extensions are processed separately...
             if (meta.isExtension) {
                 newBuilderSvc.processExtension(meta,dt,value,$scope.resource)
