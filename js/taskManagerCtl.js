9f729e6f47d4879e50dc46c52c4a22859145ea9d
davidhay25 <david.hay25@gmail.com>
2019-01-20 19:13:38 +1300

    task manager working

--- js/taskManagerCtl.js
+++ js/taskManagerCtl.js
@@ -1,25 +1,90 @@
 angular.module("sampleApp").controller('taskManagerCtrl',
-    function ($scope,$http,appConfigSvc) {
+    function ($scope,$http,appConfigSvc,$firebaseObject,$firebaseArray,$uibModal,modalService) {
 
-        let clinFhirDevice = 'Device/cfDevice';
+        $scope.firebase = firebase;
+        $scope.appConfigSvc = appConfigSvc;
+        //let clinFhirDevice = 'Device/cfDevice';
         $scope.conformanceServer = appConfigSvc.getCurrentConformanceServer();
         $scope.input = {}
 
         let fhirVersion = $scope.conformanceServer.version;     //from parent
         let taskCode =  {system:"http://loinc.org",code:"48767-8"};
         var pathExtUrl = appConfigSvc.config().standardExtensionUrl.path;  //the extension for recording the model path for a comment
-        var states = ['requested','received','accepted','rejected']
 
+        //-----------  login stuff....
+
+        $scope.login=function(){
+            $uibModal.open({
+                backdrop: 'static',      //means can't close by clicking on the backdrop.
+                keyboard: false,       //same as above.
+                templateUrl: 'modalTemplates/login.html',
+                controller: 'loginCtrl'
+            })
+        };
+
+        $scope.logout=function(){
+            firebase.auth().signOut().then(function() {
+                delete $scope.user;
+                //delete $rootScope.userProfile;
+                modalService.showModal({}, {bodyText: 'You have been logged out of clinFHIR'})
+
+            }, function(error) {
+                modalService.showModal({}, {bodyText: 'Sorry, there was an error logging out - please try again'})
+            });
+
+        };
+        //called whenever the auth state changes - eg login/out, initial load, create user etc.
+        firebase.auth().onAuthStateChanged(function(user) {
+            //if there's a hash starting with $$$ then this has been started from the project, with an authenticted user...
+            console.log('onauth',user)
+            delete $scope.user;
+
+
+            if (user) {
+                $scope.user = user;
+                //$scope.userProfile = $firebaseObject(firebase.database().ref().child("users").child(user.uid));
+                console.log($scope.user)
+
+            }
+
+        });
+
+        
+        //allow the display for a state to be different to the actual code...
+        $scope.stateHash = {};
+        $scope.stateHash.requested = 'new';
+        $scope.stateHash.received = 'reviewed';
+        $scope.stateHash.accepted = 'accepted';
+        $scope.stateHash.rejected = 'rejected';
+
+        $scope.states = [{display:'-- All statuses --',code:''}]
+        angular.forEach($scope.stateHash,function(v,k){
+            $scope.states.push({display:v,code:k});
+        });
+
+        $scope.input.filterStatus = $scope.states[0];
         $scope.model = {id:"StructureDefinition/ADRAllergyIntolerance"};    //load from a selector
 
         if (!pathExtUrl) {
-            alert("Task warning: You must restart clinFHIR then the Logical Modeller to reset updated config")
+            alert("Task warning: You must restart clinFHIR then the Task Manager to reset updated config")
         }
 
-        //add a new not as an annotation
+        //for the task list filter...
+        $scope.canShowTask = function(task,filter) {
+            if (filter.code === "") {
+                return true
+            } else if (task.status == filter.code) {
+                return true;
+            }
+            return false;
+        };
+
+        //add a new note as an annotation
         $scope.addNote = function(note) {
-            $scope.localTask.notes = $scope.localTask.notes || []
+            $scope.localTask.notes = $scope.localTask.notes || [];
+
             var annot = {text:note,time: new Date().toISOString()};
+            annot.authorString = $scope.user.email;
             $scope.localTask.notes.push(annot);
             delete $scope.input.note;
 
@@ -43,8 +108,37 @@ angular.module("sampleApp").controller('taskManagerCtrl',
             $scope.fhirTask = task.resource
             $scope.localTask = angular.copy(task)
             delete $scope.localTask.resource;
-        }
+        };
 
+        
+        $scope.showStateChange = function(newState,currentState) {
+            switch (newState) {
+                case 'received' :
+                    if (currentState == 'requested') {return true}
+                    break;
+                case 'accepted' :
+                    if (currentState == 'requested' || currentState == 'received') {return true}
+                    break;
+                case 'declined' :
+                    if (currentState == 'requested' || currentState == 'received') {return true}
+                    break;
+            }
+        };
+
+        $scope.changeState = function(newState) {
+            $scope.selectedTask.status = newState;
+            $scope.localTask.status = newState;
+            $scope.fhirTask.status = newState;
+            let url = $scope.conformanceServer.url + "Task/"+$scope.fhirTask.id;    //from parent controller
+            $http.put(url,$scope.fhirTask).then(
+                function() {
+
+                }, function (err) {
+                    alert(angular.toJson(err))
+                }
+            )
+        };
+        
         //load all the tasks for a given model
         function loadTasksForModel(id) {
             $scope.tasks = []
@@ -52,7 +146,6 @@ angular.module("sampleApp").controller('taskManagerCtrl',
             url += "?code="+taskCode.system +"|"+taskCode.code;
             url += "&_count=100";    //todo - need the follow links
 
-            //url += '?requester='+clinFhirDevice;
             $http.get(url).then(
                 function(data) {
                     console.log(data)
@@ -62,7 +155,9 @@ angular.module("sampleApp").controller('taskManagerCtrl',
                             let task = {}       //internal task
                             //task.resource = resource;       //for degugging...
                             task.description = resource.description;
-                            task.notes = resource.note
+                            task.notes = resource.note;
+
+                            task.status = resource.status || 'requested';
 
                             if (resource.requester) {
                                 switch (fhirVersion) {
@@ -81,7 +176,6 @@ angular.module("sampleApp").controller('taskManagerCtrl',
                                 }
                             }
 
-
                             let extSimpleExt = getSingleExtensionValue(resource, pathExtUrl);
                             if (extSimpleExt) {
                                 task.path = extSimpleExt.valueString;
@@ -90,7 +184,6 @@ angular.module("sampleApp").controller('taskManagerCtrl',
                             task.resource = resource;       //for degugging...
 
 
-                            console.log(task);
                             $scope.tasks.push(task)
                         })
                     }
@@ -105,7 +198,7 @@ angular.module("sampleApp").controller('taskManagerCtrl',
 
         loadTasksForModel($scope.model.id);
 
-        $scope.addTask = function() {
+        $scope.addTaskDEP = function() {
             let comment = window.prompt('Enter comment');       //todo make dialog
             if (comment) {
                 let task = {resourceType:'Task'};
