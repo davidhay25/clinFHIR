c139af4c837fc54b426c5a38185fb6fa2581b82e
davidhay25 <david.hay25@gmail.com>
2019-11-28 19:39:16 +0000

    working on mapping UI

--- js/mappingCtl.js
+++ js/mappingCtl.js
@@ -3,7 +3,7 @@ angular.module("sampleApp")
         function ($scope,$http,v2ToFhirSvc,$uibModal,$timeout,modalService) {
             $scope.input = {};
             $scope.serverRoot = "https://vonk.fire.ly/";
-            $scope.structureMapId = 'dh';
+           // $scope.structureMapId = 'dh';
             $scope.input.isDirty = false;
 
             //todo - should these move to app config svc???
@@ -24,14 +24,18 @@ angular.module("sampleApp")
             }
 
             function selectMap(map) {
-
-                $scope.currentSM = map
-
-                $scope.input.mappingFile = getStringExtension($scope.currentSM,extMapUrl)[0];
-                $scope.input.inputJson = getStringExtension($scope.currentSM,extExampleUrl)[0];
-
+                delete $scope.input.mappingFile, $scope.input.inputJson;
+                //$scope.currentSM = map;
+                let mf = getStringExtension(map,extMapUrl);
+                if (mf.length > 0) {
+                    $scope.input.mappingFile = mf[0]
+                }
+                let ex = getStringExtension(map,extExampleUrl);
+                if (ex.length > 0) {
+                    $scope.input.inputJson = ex[0]
+                }
             }
-          //  loadSM($scope.structureMapId);
+
 
             $scope.maps = [];
             firebase.auth().onAuthStateChanged(function(user) {
@@ -40,6 +44,7 @@ angular.module("sampleApp")
                     console.log(user)
 
                     let dUrl = $scope.serverRoot + "StructureMap?publisher="+user.email
+                    $scope.showWaiting = true;
                     $http.get(dUrl).then(
                         function(data) {
                             console.log(data.data);
@@ -54,11 +59,13 @@ angular.module("sampleApp")
                             //$scope.input.mappingFile = getStringExtension($scope.currentSM,extMapUrl)[0];
                             //$scope.input.inputJson = getStringExtension($scope.currentSM,extExampleUrl)[0];
                             console.log($scope.maps)
-                            $scope.input.model = $scope.maps[0]
-                            selectMap($scope.input.model);
-
+                            $scope.currentSM = $scope.maps[0]
+                            //$scope.input.model = $scope.maps[0]     //todo input.model is what the selector DD is bound to - is this right??
+                            selectMap($scope.currentSM);      //sets currentSM and the map and json vars from the SM
                         }
-                    );
+                    ).finally(function () {
+                        $scope.showWaiting = false;
+                    });
 
                 } else {
                     delete $scope.user
@@ -83,28 +90,74 @@ angular.module("sampleApp")
                 });
             };
 
-           // $scope.selectMap = function(map){
-             //   console.log(map)
-          //  }
+            //when a map is selected from the drop down list for this user
+            $scope.selectMapFromDD = function(map){
+                console.log(map)
+                selectMap(map)
+
+            };
 
             $scope.addMap = function(map){
                 $uibModal.open({
                     backdrop: 'static',      //means can't close by clicking on the backdrop.
                     keyboard: false,       //same as above.
+                    size:'lg',
                     templateUrl: 'modalTemplates/addMap.html',
-                    controller: function(){
+                    controller: function($scope,$http,serverRoot){
+                        $scope.validId = false;
+                        $scope.checkId = function(){
+                            let url = serverRoot+"StructureMap/"+$scope.id
+                            $http.get(url).then(
+                                function(){
+                                    alert("Sorry, this Id is already in use")
+                                },
+                                function(err){
+                                    console.log(err);
+                                    if (err.status == 404) {
+                                        $scope.validId = true;
+                                    } else {
+                                        alert("Error contacting server:"+angular.toJson(err))
+                                    }
+                                }
+                            )
+                        };
+
+                        $scope.add = function(){
+                            $scope.$close({id:$scope.id,sample:$scope.sample,description:$scope.description,name:name})
+                        }
+
+                    },resolve : {
+                        serverRoot: function () {          //the default config
+                            return $scope.serverRoot;
+
+                        }
+                    }
+                }).result.then(function(vo){
+                    console.log(vo)
+                   // $scope.structureMapId = vo.id;      //the id of the current map
+                    if (vo.sample) {
+                        //set the sample data
+                        $scope.input.inputJson = $scope.sample.inputJson;
+                        $scope.input.mappingFile = $scope.sample.mappingFile;
+                        //create the actual StructureMap resource
+                        $scope.updateStructureMap(function(map){
+                            //after the map has been created, we add it to the list of maps and make it current...
+                            $scope.maps.push(map);
+                            $scope.input.model = map;   //for the dropdown
+                            //$scope.currentSM = map;
+                        });
+                    } else {
 
                     }
                 })
             };
-            //extension urls
 
-           /*
-            //get the mapping file (text)
+            //get the sample mapping file (text)
             let urlMap = "artifacts/mapping/fakemeds.txt";
+            $scope.sample = {};
             $http.get(urlMap).then(
                 function(data) {
-                    $scope.input.mappingFile = data.data
+                    $scope.sample.mappingFile = data.data
                 }
             );
 
@@ -112,20 +165,21 @@ angular.module("sampleApp")
             let urlInput = "artifacts/mapping/fakemedChart.json";
             $http.get(urlInput).then(
                 function(data) {
-                    $scope.input.inputJson = angular.toJson(data.data,true)
+                    $scope.sample.inputJson = angular.toJson(data.data,true)
                 }
             );
-*/
-            $scope.validate = function() {
 
+            $scope.validate = function() {
+                alert('validate not yet enabled')
             };
 
             $scope.selectEntryFromBundle = function(entry){
                 $scope.selectedEntry = entry
             };
 
-            //convert the map into an SM resource using $convert, then update the resource if the conversion was successful...
-            $scope.updateStructureMap = function() {
+            //convert the map into an SM resource using $convert, then update the SM resource if the conversion was successful...
+            $scope.updateStructureMap = function(cb) {
+                $scope.showWaiting = true;
                 delete $scope.convertError, $scope.transformError;
                 let url = $scope.serverRoot + "StructureMap/$convert";
                 let options = {headers:{}};
@@ -135,30 +189,51 @@ angular.module("sampleApp")
                     function(data) {
                         let structureMapResource = data.data;
                         console.log(structureMapResource)
-                        structureMapResource.id = $scope.structureMapId;
+                        //structureMapResource.id = $scope.structureMapId;
+                        structureMapResource.id = $scope.currentSM.id;
                         structureMapResource.publisher = $scope.user.email;
+                        structureMapResource.description = $scope.currentSM.description;
+                        structureMapResource.name = $scope.currentSM.name;
                         //now add the map and the example file/s to the resource as extensions
 
                         //todo need to add sdefs to server...
                         addStringExtension(structureMapResource,extMapUrl,$scope.input.mappingFile);
                         addStringExtension(structureMapResource,extExampleUrl,$scope.input.inputJson);
 
-
-
                         console.log(data.data)
-                        let url = $scope.serverRoot + "StructureMap/" + $scope.structureMapId;
+                        let url = $scope.serverRoot + "StructureMap/" + $scope.currentSM.id;
+                        //let url = $scope.serverRoot + "StructureMap/" + $scope.structureMapId;
                         $http.put(url,structureMapResource).then(
                             function() {
                                 alert("StructureMap updated")
                                 $scope.input.isDirty = false;
+                                //$scope.currentSM = structureMapResource;
+
+                                //update the maps array
+                                for (var i=0; i < $scope.maps.length-1; i++) {
+                                    if ($scope.maps[i].id == structureMapResource.id) {
+                                        $scope.maps[i] = structureMapResource;
+                                        $scope.currentSM = $scope.maps[i];
+                                        break;
+                                    }
+                                }
+
+
+
+                                if (cb) {
+                                    cb(structureMapResource)
+                                }
                             }, function(err) {
                                 $scope.convertError = err.data;
                             }
-                        )
+                        ).finally(function () {
+                            $scope.showWaiting = false;
+                        })
                     },
                     function(err) {
                         console.log(err);
                         $scope.convertError = err.data
+                        $scope.showWaiting = false;
 
                     }
                 )
@@ -188,14 +263,14 @@ angular.module("sampleApp")
 
             };
 
-
             //perform the actual expansion...
             $scope.transform = function(){
                 delete $scope.output;
                 delete $scope.convertError, $scope.transformError;
                 $scope.transformMessage = "Executing map on server, please wait...";
                 //https://vonk.fire.ly/StructureMap/dh/$transform
-                let url = $scope.serverRoot + "StructureMap/" + $scope.structureMapId + "/$transform";
+                //let url = $scope.serverRoot + "StructureMap/" + $scope.structureMapId + "/$transform";
+                let url = $scope.serverRoot + "StructureMap/" + $scope.currentSM.id + "/$transform";
                 let input = $scope.input.inputJson;
                 $http.post(url,input).then(
                     function(data) {
