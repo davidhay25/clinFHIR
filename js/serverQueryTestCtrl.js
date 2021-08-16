617f65b8e4ea07af0f9cc29f8f96322ade99fcd5
davidhay25 <david.hay25@gmail.com>
2021-08-17 06:28:24 +1200

    support v2 post

--- js/serverQueryTestCtrl.js
+++ js/serverQueryTestCtrl.js
@@ -89,46 +89,20 @@ angular.module("sampleApp")
                 delete  $scope.selectedTestResource
                 delete $scope.updateResponse;
                 $scope.selectedTest = angular.copy(test)
-                /*
-                if ($scope.selectedTest.resource) {
-                    let json1 = $scope.selectedTest.resource
-                    let json2 = JSON.stringify(JSON.parse(json1),null,2)
+                if ($scope.selectedTest.v2) {
+                    let segments = []
+                    const regex = /|/
+                    $scope.selectedTest.v2.forEach(function (segment){
+                        let lne = segment.split("|").join("| ")
+                        segments.push(lne.split("^").join("^ "))
+                    })
+                    $scope.selectedTest.v2 = segments
 
-                    $scope.selectedTestResource = json2
                 }
-*/
-                //let fullQuery = $scope.script.server + test.query;
+
+
+
 
             }
-/*
-            //retrieve a test script. Maybe store in local cache and hove UI to paste one in (in json)...
-            $scope.getTestScript = function() {
-                //server is default server - or use selected one????
-                //maybe if there is a server in the script it forces that server to be selected
-                let script = {server:'http://hapi.fhir.org/baseR4/',tests:[]}
-                script.patientIdentifier = "http://www.clinfhir.com/base|abc1234";  //the patient to test against
 
-                let test = {name:'single code',description:'Get all results for a single test'}
-                test.query = "Observation?patient.identifier={identifier}&code=http://loinc.org|26464-8"
-                test.expected = {note:"A single observation"}
-                script.tests.push(test)
-
-                let test1 = {name:'DR include observations',description:'Get all results in the past year including observations'}
-                test1.query = "DiagnosticReport?patient.identifier={identifier}&code=http://loinc.org|58410-2&date=ge2021-01-01&_include=DiagnosticReport:result"
-                test1.expected = {note:"One DR & 2 Observations"}
-                script.tests.push(test1)
-
-                let test2 = {name:'FBC include observations & ordering provider',description:'All FBC panels including Observations and ordering provider'}
-                test2.query = "DiagnosticReport?patient.identifier=http://www.clinfhir.com/base|abc1234&code=http://loinc.org|58410-2&_include=DiagnosticReport:result&_include=DiagnosticReport:based-on&_include:iterate=ServiceRequest:requester"
-                test2.expected = {note:"One DR & 2 Observations"}
-                script.tests.push(test2)
-
-                $scope.script = script;
-
-                console.log(angular.toJson(script))
-
-            }
-            $scope.getTestScript();
-
-            */
         })
