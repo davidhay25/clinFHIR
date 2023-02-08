082bd823c47a7f687bfc6186cb36e8c196c5b0cc
david hay <davidhay@davids-air.lan>
2023-02-08 13:45:48 +1300

    filters working

--- serverModuleQA.js
+++ serverModuleQA.js
@@ -20,12 +20,17 @@ function setup(app) {
 
         req.on('end', function () {
 
-            console.log('body',body)
-            let index = JSON.parse(fs.readFileSync('./artifacts/qaindex.json'))
-            let entry = index[body.id]
+            let json = (JSON.parse(body))
+            //console.log('body',body)
+            let index = JSON.parse(fs.readFileSync('./artifacts/qaindex.json').toString())
+            console.log(json)
+            console.log(json.id)
+            let entry = index[json.id]
+            console.log('entry',entry)
             if (entry) {
-                entry.name = body.name
-                fs.writeFileSync('./artifacts/qaindex.json',JSON.stringify(index))
+                entry.claimedby = json.name
+                console.log('entry1',entry)
+                fs.writeFileSync('./artifacts/qaindex.json',JSON.stringify(index,null,2))
 
             }
             res.json(index)
