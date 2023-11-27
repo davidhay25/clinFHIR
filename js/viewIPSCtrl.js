49b135046f8d84ecd73db14b7d7fe34e009ea6ae
David Hay <david.hay25@gmail.com>
2023-11-28 06:43:44 +1300

    updated viewer

--- js/viewIPSCtrl.js
+++ js/viewIPSCtrl.js
@@ -10,17 +10,34 @@ angular.module("sampleApp")
         let ipsProfile = "http://hl7.org/fhir/uv/ips/StructureDefinition/Bundle-uv-ips"
         let nhiSystem = "https://standards.digital.health.nz/ns/nhi-id"
 
-        $scope.config = []
-        $scope.config.push({name:"Terminz",baseUrl:"https://terminz.azurewebsites.net/fhir/"})
+        $scope.endPoints = []
+
+        let tnzConfig = {name:"Terminz test",baseUrl:"https://terminz.azurewebsites.net/fhir/"}
+        tnzConfig.nhi = "https://standards.digital.health.nz/ns/nhi-id|NNJ9186"
+        $scope.endPoints.push(tnzConfig)
+
+        let tnzConfig1 = {name:"Iosefa Test-Fuimaono",baseUrl:"https://terminz.azurewebsites.net/fhir/"}
+        tnzConfig1.nhi = "https://standards.digital.health.nz/ns/nhi-id|ZKT9319"
+        $scope.endPoints.push(tnzConfig1)
+
+        $scope.input.endpoint = $scope.endPoints[0]
+
 
         $scope.to_trusted = function(html_code) {
             return $sce.trustAsHtml(html_code);
         }
 
-        $scope.load = function (nhi) {
-            let server = $scope.config[0]
+        $scope.selectEP = function (ep) {
+            delete $scope.composition
+            delete $scope.hashResources
+            delete $scope.selectedResource
+            delete $scope.OO
+        }
 
-            $scope.qry = `${server.baseUrl}Patient/$summary?profile=${ipsProfile}&identifier=${nhiSystem}|${nhi}&_format=json`
+        $scope.load = function (endPoint) {
+
+
+            $scope.qry = `${endPoint.baseUrl}Patient/$summary?profile=${ipsProfile}&identifier=${endPoint.nhi}&_format=json`
 
             $scope.showWaiting = true
             $http.get($scope.qry).then(
@@ -38,16 +55,40 @@ angular.module("sampleApp")
             //https://terminz.azurewebsites.net/fhir/Patient/$summary?profile=http://hl7.org/fhir/uv/ips/StructureDefinition/Bundle-uv-ips&identifier=https://standards.digital.health.nz/ns/nhi-id|ZKT9319&_format=json
 
         }
+        
+        $scope.selectResource = function (ref) {
+            $scope.selectedResource = $scope.hashResources[ref.reference]
+        }
+
+        $scope.validate = function (resource) {
+            let url = `http://hapi.fhir.org/baseR4/${resource.resourceType}/$validate`
+            $scope.showWaiting = true
+            $http.post(url,resource).then(
+                function (data) {
+                    $scope.OO = data.data
+
+                    $scope.showWaiting = false
+                }, function (err) {
+                    $scope.showWaiting = false
+                    $scope.OO = err.data
+                }
+            )
+
+        }
 
         function processIPS(bundle) {
             if (bundle.entry) {
-                let hashResources = {}
+                $scope.hashResources = {}
                 $scope.composition = bundle.entry[0].resource
                 $scope.entries = []
                 bundle.entry.forEach(function (entry) {
                     let resource = entry.resource
-                    let ref = `${resource.resourceType/resource.id}`
-                    hashResources[ref] = resource
+                    let ref = `${resource.resourceType}/${resource.id}`
+                    $scope.hashResources[ref] = resource
+
+                    if (resource.resourceType == 'Patient') {
+                        $scope.patient = resource
+                    }
                 })
 
                 $scope.composition.section.forEach(function (section) {
